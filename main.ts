95231a7a0e1dd402b24e38290dfb681aed047360
JU4Nkpl <130647266+JU4Nkpl@users.noreply.github.com>
2023-05-08 08:51:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Yes)
+    } else if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Angry)
+    } else if (receivedNumber == 3) {
+        basic.showIcon(IconNames.Surprised)
+    } else if (receivedNumber == 4) {
+        basic.showIcon(IconNames.Ghost)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("smile")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("5")
+})
+radio.setGroup(127)
